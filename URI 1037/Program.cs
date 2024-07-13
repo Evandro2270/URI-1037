0fc1827e9c38b436e270a9c32efc45e0c16a07d4
Evandro2270 <evandro535@gmail.com>
2024-07-13 14:08:20 -0300

    Adicionar arquivos de projeto.

---
+++ URI 1037/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double numero = double.Parse(Console.ReadLine(), CI);
+
+                if (numero < 0.0 || numero > 100.0)
+                {
+                    Console.WriteLine("Fora de intervalo");
+                }
+                else if (numero <= 25.0)
+                {
+                    Console.WriteLine("intervalo [0,25]");
+                }
+                else if (numero <= 50.0)
+                {
+                    Console.WriteLine("intervalo [25,50]");
+                }
+
+                else if (numero <= 75.0)
+                {
+                    Console.WriteLine("intervalo [50,75]");
+                }
+
+                else
+                {
+                    Console.WriteLine("intervalo [75,100]");
+                }
+
+            }
+        }
+    }
+}
